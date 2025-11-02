ebcd3ad574d23d5351ab37bb966453332eb0beb1
Collin Cleary <collin.cleary@yahoo.com>
2025-11-02 14:22:27 -0500

    trying again

--- resources/pytorch_Unet_experiment.py
+++ resources/pytorch_Unet_experiment.py
@@ -1,4 +1,204 @@
+import os
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+
+
+class CarSegmentationDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None, target_transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.images = sorted(os.listdir(image_dir))
+        self.masks = sorted(os.listdir(mask_dir))
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.masks[idx])
+
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")  # grayscale mask
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            mask = self.target_transform(mask)
+
+        mask = (mask > 0).float()  # convert to binary mask
+        return image, mask
+
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1):
+        super().__init__()
+        self.down1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.down2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.down3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        self.down4 = DoubleConv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        self.middle = DoubleConv(512, 1024)
+
+        self.up1 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
+        self.conv1 = DoubleConv(1024, 512)
+        self.up2 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.conv2 = DoubleConv(512, 256)
+        self.up3 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.conv3 = DoubleConv(256, 128)
+        self.up4 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.conv4 = DoubleConv(128, 64)
+
+        self.out = nn.Conv2d(64, out_channels, 1)
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        p1 = self.pool1(d1)
+        d2 = self.down2(p1)
+        p2 = self.pool2(d2)
+        d3 = self.down3(p2)
+        p3 = self.pool3(d3)
+        d4 = self.down4(p3)
+        p4 = self.pool4(d4)
+
+        m = self.middle(p4)
+
+        u1 = self.up1(m)
+        u1 = torch.cat([u1, d4], dim=1)
+        u1 = self.conv1(u1)
+        u2 = self.up2(u1)
+        u2 = torch.cat([u2, d3], dim=1)
+        u2 = self.conv2(u2)
+        u3 = self.up3(u2)
+        u3 = torch.cat([u3, d2], dim=1)
+        u3 = self.conv3(u3)
+        u4 = self.up4(u3)
+        u4 = torch.cat([u4, d1], dim=1)
+        u4 = self.conv4(u4)
+
+        return self.out(u4)
+
+
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])
+
+target_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])
+
+
+full_dataset = CarSegmentationDataset(
+    image_dir="./dataset/cars/img",
+    mask_dir="./dataset/cars/masks",
+    transform=transform,
+    target_transform=target_transform
+)
+
+
+train_size = int(0.8 * len(full_dataset))
+val_size = len(full_dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+
+
+
+model = UNet().to(device)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)#1e-4)
+
+num_epochs = 20
+train_losses = []
+
+for epoch in range(num_epochs):
+
+    model.train()
+    running_loss = 0.0
+    for images, masks in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+        images, masks = images.to(device), masks.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, masks)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * images.size(0)
+
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for images, masks in val_loader:
+            images, masks = images.to(device), masks.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, masks)
+            val_loss += loss.item() * images.size(0)
+    val_loss /= len(val_loader.dataset)
+
+    print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {train_loss:.4f}, Val loss: {val_loss:.4f}")
+
+
+model.eval()
+with torch.no_grad():
+    img, mask = val_dataset[0]
+    img_tensor = img.unsqueeze(0).to(device)
+    pred = torch.sigmoid(model(img_tensor)).squeeze().cpu().numpy()
+    pred_binary = (pred > 0.5).astype(np.float32)
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 3, 1)
+plt.imshow(np.transpose(img.numpy(), (1, 2, 0)))
+plt.title("Image")
+
+plt.subplot(1, 3, 2)
+plt.imshow(mask[0], cmap='gray')
+plt.title("True Mask")
+
+plt.subplot(1, 3, 3)
+plt.imshow(pred_binary, cmap='gray')
+plt.title("Predicted Mask")
+
+plt.show()
