f38755fc634da3fb7aa6707a05544e0c0767ff23
Collin Cleary <collin.cleary@yahoo.com>
2025-11-02 01:16:23 -0500

    trying this one last time

--- resources/pytorch_tutorial.py
+++ resources/pytorch_tutorial.py
@@ -9,3 +9,127 @@ import timm
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+class PlayingCardDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data = ImageFolder(data_dir, transform=transform)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return  self.data[idx]
+    
+    @property
+    def classes(self):
+        return self.data.classes
+    
+class SimpleCardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained = True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+        enet_out_size = 1280
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+    
+
+data_dir= './cards-image-datasetclassification/train'
+dataset = PlayingCardDataset(data_dir)
+
+# print(len(dataset))
+# print(dataset[5])
+
+
+target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
+
+# print(target_to_class)
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+
+# for image, label in dataset:
+#     break
+
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+# for images, labels in dataloader:
+#     break
+
+
+
+# print(str(model)[:500])
+
+
+
+
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+train_folder = './cards-image-datasetclassification/train'
+valid_folder = './cards-image-datasetclassification/valid'
+test_folder = './cards-image-datasetclassification/test'
+
+train_dataset = PlayingCardDataset(train_folder, transform=transform)
+val_dataset = PlayingCardDataset(valid_folder, transform=transform)
+test_dataset = PlayingCardDataset(test_folder, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+
+
+num_epoch = 5
+train_losses, val_losses = [], []
+
+model = SimpleCardClassifier(num_classes=53).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+for epoch in range(num_epoch):
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc='Training loop'):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * labels.size(0)
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader, desc='Validation loop'):
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * labels.size(0)
+    val_loss = running_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+
+    print(f"Epoch {epoch+1}/{num_epoch} - Train loss: {train_loss}, Validation loss: {val_loss}")
